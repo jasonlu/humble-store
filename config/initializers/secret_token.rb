b600bb2a53920b9d22973661c5fd23f59a1616cc
Jason Lu <jasonl.biz@gmail.com>
2013-10-16 19:34:05 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HumbleStore::Application.config.secret_key_base = 'c38d2795d0912d6e0aaa39f7a525a962c499f1c9e5f19edb82a3885f5cbc551bca4cdd3e9b7721e246d49357a8006f56beb77183bf3d3a725c3d918e6fea8ab9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HumbleStore::Application.config.session_store :cookie_store, key: '_humble-store_session'
